6f4aa06b4cb570539c66a5fdaaeb650aa0bc93f3
NikTheGeek1 <ntheodoropoulos@outlook.com>
2026-01-20 16:29:23 +0200

    waiting 3 seconds for UI elements to update

--- marty-blocks-lib/replacements/scratch-gui/src/components/marty-save-load/local-storage/local-storage.jsx
+++ marty-blocks-lib/replacements/scratch-gui/src/components/marty-save-load/local-storage/local-storage.jsx
@@ -126,7 +126,7 @@ class SaveLoad extends React.Component {
       setTimeout(() => {
         // give some time so all the buttons/devices are rendered before we start updating the UI
         window.raftManager.onProjectLoaded();
-      }, 2000);
+      }, 3000);
       alert(this.props.intl.formatMessage(messages.projectLoaded));
       // this seems to be required to let the wm load the project
       window.setTimeout(() => this.props.onActivateBlocksTab());
